4175ebc89f49046b8822b2b9ece2be2d26627330
Vitalya Shevtsov <kaktusyaka@gmail.com>
2014-01-16 17:32:51 +0200

    started login method

--- lib/apple_bot.rb
+++ lib/apple_bot.rb
@@ -1,5 +1,19 @@
 require "apple_bot/version"
+require 'mechanize'
 
 module AppleBot
   # Your code goes here...
+  class SignIn
+    class << self
+      def login
+        agent = WWW::Mechanize.new
+        agent.get("https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa")
+        puts 'logged'
+      end
+
+      def logout
+        puts 'logout'
+      end
+    end
+  end
 end
