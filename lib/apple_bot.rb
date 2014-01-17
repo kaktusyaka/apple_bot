db065b581f2199a4dac5ed45bddd7d6949302272
Vitalya Shevtsov <kaktusyaka@gmail.com>
2014-01-17 11:16:05 +0200

    added able login and logout

--- lib/apple_bot.rb
+++ lib/apple_bot.rb
@@ -3,16 +3,29 @@ require 'mechanize'
 
 module AppleBot
   # Your code goes here...
-  class SignIn
+  class Login
     class << self
       def login
-        agent = WWW::Mechanize.new
-        agent.get("https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa")
-        puts 'logged'
+        @agent = Mechanize.new
+        user = 'sergei.zenchenko@gmail.com'
+        pass = 'K<6{72AZ?Y3Hu]Er'
+        @agent.get("https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa") do |page|
+          form = page.form_with(name: 'appleConnectForm') do |f|
+            f.theAccountName = user
+            f.theAccountPW   = pass
+          end.submit
+          if form.body.include?('Manage Your Apps')
+            return form
+          else
+            return false
+          end
+        end
       end
 
       def logout
-        puts 'logout'
+        page = login
+        result = @agent.click(page.link_with(text: 'Sign Out'))
+        return !result.links.include?('Sign Out')
       end
     end
   end
