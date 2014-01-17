2710dea0aa6cfe44079751d58868e21a51ce08d5
Vitalya Shevtsov <kaktusyaka@gmail.com>
2014-01-17 14:54:26 +0200

    added able receive users list from apple

--- lib/apple_bot.rb
+++ lib/apple_bot.rb
@@ -2,13 +2,20 @@ require "apple_bot/version"
 require 'mechanize'
 
 module AppleBot
-  # Your code goes here...
+  class Client
+    class << self
+      def agent
+        @agent ||= Mechanize.new
+      end
+    end
+  end
+
   class Authorization
     class << self
       def login
         user = 'sergei.zenchenko@gmail.com'
         pass = 'K<6{72AZ?Y3Hu]Er'
-        agent.get("https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa") do |page|
+        AppleBot::Client.agent.get("https://itunesconnect.apple.com/WebObjects/iTunesConnect.woa") do |page|
           return page unless page.body.include?("Sign In")
           form = page.form_with(name: 'appleConnectForm') do |f|
             f.theAccountName = user
@@ -20,13 +27,24 @@ module AppleBot
 
       def logout
         page = login
-        result = agent.click(page.link_with(text: 'Sign Out'))
+        result = AppleBot::Client.agent.click(page.link_with(text: 'Sign Out'))
         return !result.links.include?('Sign Out')
       end
+    end
+  end
 
-      private
-      def agent
-        @agent ||= Mechanize.new
+  class ManageUsers
+    class << self
+      def get_list
+        page = AppleBot::Authorization.login
+        page = AppleBot::Client.agent.click(page.link_with(text: 'Manage Users'))
+        users = page.search('table.lcbox table a')
+        return 'No users' if users.count.zero?
+        users = []
+        page.search('table.lcbox table a').each do |user|
+          users << { name: user.text, url: user.attributes['href'].value }
+        end
+        users.to_json
       end
     end
   end
